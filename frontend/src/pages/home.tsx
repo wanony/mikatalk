a6e4c897a5d47f20a08dce22a0ee43d8bc28e0a8
wanony <65881470+wanony@users.noreply.github.com>
2023-12-10 16:01:42 +0800

    Update homepage to feature some pro-mika quotes

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import HomeTypeAnimation from "../components/typers/HomeTypeAnimation";
 
 const Home = () => {
-  return <div>Home</div>;
+  // const theme = useTheme();
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+          mt: 3,
+        }}
+      >
+        <Box>
+          <HomeTypeAnimation />
+        </Box>
+        {/* <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: { md: "row", xs: "column", sm: "column" },
+            gap: 5,
+            my: 10,
+          }}
+        >
+          <img
+            src="robot.png"
+            alt="robot"
+            style={{ width: "200px", margin: "auto" }}
+          />
+          <img
+            className="image-inverted rotate"
+            src="openai.png"
+            alt="openai"
+            style={{ width: "200px", margin: "auto" }}
+          />
+        </Box>
+        <Box sx={{ display: "flex", mx: "auto" }}>
+          <img
+            src="chat.png"
+            alt="chatbot"
+            style={{
+              display: "flex",
+              margin: "auto",
+              width: isBelowMd ? "80%" : "60%",
+              borderRadius: 20,
+              boxShadow: "-5px -5px 105px #64f3d5",
+              marginTop: 20,
+              marginBottom: 20,
+              padding: 10,
+            }}
+          />
+        </Box>
+      </Box>
+      <Footer /> */}
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
